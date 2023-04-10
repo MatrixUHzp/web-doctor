b31e6f67aec98e64fad2576af9c906f3611cf40e
Matias <Matias Rasmussen>
2023-04-10 15:49:04 -0300

    Agregue herencias de templates y linkie algunas vistas

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from inicio import views
 
-app_name = 'inicio'
+# app_name = 'inicio'
 
 urlpatterns = [
-    path('', views.mi_vista),
-    path('crear-medico/', views.crear_medico),
+    path('', views.mi_vista, name='inicio'),
+    path('crear-medico/', views.crear_medico, name='doctor'),
+    path('crear-paciente/', views.crear_paciente, name='paciente')
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from inicio.models import Medico
+from inicio.models import Paciente
 
 def mi_vista(request):
     return render(request, r'inicio/index.html')
 
 def crear_medico(request):
     medico1 = Medico(nombre='Marisel', edad=32)
-    medico1.save()
+    # medico1.save()
     datos = {'medico': medico1}
     return render(request, r'inicio/crear_medico.html', datos)
+
+def crear_paciente(request):
+    paciente1 = Paciente(nombre='Matias', apellido='Rasmussen', fecha_nacimiento = 1707)
+    # paciente1.save()
+    datos = {'paciente': paciente1}
+    return render(request, r'inicio/crear_paciente.html', datos)
+
