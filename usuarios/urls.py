b43f73a3577d126e388b6b681c5861e7a35be405
Matias <Matias Rasmussen>
2023-05-24 23:06:06 -0300

    Correccion de errores 4

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from usuarios import views
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
 
 # app_name = 'usuario'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('registro/', views.registro, name='registro'),
-    path('editar-perfil/', views.editar_perfil, name='editar_perfil'),
+    path('editar-perfil/', login_required(views.editar_perfil), name='editar_perfil'),
     path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
     path('cambio-contraseña/', views.CambioContraseña.as_view(), name='cambio_contraseña'),
 
